740fe3029bf773f8eefdf5ebfee7e4598fd0c127
pakalapati <adityapakalapati@hotmail.com>
2023-10-03 23:17:28 +1100

    removed popup and added no photo flag pin

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Website to show photos on maps.
+
+To run locally:
+npm install 
+npm start
+
+To deploy:
+npm run deploy
--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import { Icon } from 'leaflet';
 import './App.scss';
 import './leaflet.css'
 import { MapContainer, TileLayer, Marker, Popup, Tooltip } from 'react-leaflet';
-import { useEffect, useState, TouchEvent } from 'react';
+import { useEffect, useState } from 'react';
 import {isMobileOS} from './commonUtils';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
 
 function App() {
 
   const [images, setImages] = useState<any[]>([]);
   const [open, setOpen] = useState<Boolean>(false);
-  const [touchStart, setTouchStart] = useState(0);
-  const [touchEnd, setTouchEnd] = useState(0);
   const [imageSource, setImageSource] = useState<string>('');
   const [imageCaption, setImageCaption] = useState<string>('');
 
   const photoClicked = (photoIndex: number) => {
-    if(photoIndex >= 0){
+    if(photoIndex >= 0 && images[photoIndex].imageLink){
       setImageSource(images[photoIndex].imageLink);
       setImageCaption(images[photoIndex].title);
       setOpen(true);
@@ -40,34 +40,37 @@ function App() {
     getPhotos();
   }, []);
 
-  const customIcon = new Icon({
+  const noPhotoFlag = new Icon({
+    iconUrl: "./no-photo-flag.svg",
+    iconSize: [38, 38]
+  });
+
+  const photoFlag = new Icon({
     iconUrl: "./flag.svg",
     iconSize: [38, 38]
   });
 
   return (
-    <div>
-      <div id="map">
-        <MapContainer center={isMobileOS() ? [0, -86.23992182162134] : [0, 0]} zoom={isMobileOS() ? 2 : 3} scrollWheelZoom={true} minZoom={isMobileOS() ? 2 : 3} worldCopyJump={true}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {images.map((photo: any, index: number) => 
-              <Marker position={photo.latlong} icon={customIcon} key={index}>
-              { photo.thumbnailLink && <Popup>
-                <img src={photo.thumbnailLink} alt={photo.title} className="popUpImage" onClick={() => photoClicked(index)}/>
-              </Popup> }
-              <Tooltip>{photo.title}</Tooltip>
-            </Marker>
-            )} 
-        </MapContainer>      
+      <div>
+        <div id="map">
+          <MapContainer center={isMobileOS() ? [0, -86.23992182162134] : [0, 0]} zoom={isMobileOS() ? 2 : 3} scrollWheelZoom={true} minZoom={isMobileOS() ? 2 : 3} worldCopyJump={true}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {images.map((photo: any, index: number) => 
+                <Marker position={photo.latlong} icon={photo.imageLink ? photoFlag : noPhotoFlag} key={index} eventHandlers={{ click: function(){ photoClicked(index) }}}>
+                <Tooltip>{photo.title}</Tooltip>
+              </Marker>
+              )} 
+          </MapContainer>      
+        </div>
+        <div className={open ? "fullScreenModal fullScreen" : "fullScreenModal"} style={{ backgroundImage: `url(${imageSource})` }}>
+          <span className={open ? "fullScreenModal__close fullScreen" : "fullScreenModal__close"} onClick={toggle}>&times;</span>
+          <div className={open ? "fullScreenModal__caption fullScreen" : "fullScreenModal__caption"}>{imageCaption}</div>
+        </div>
       </div>
-      <div className={open ? "fullScreenModal fullScreen" : "fullScreenModal"} style={{ backgroundImage: `url(${imageSource})` }}>
-        <span className={open ? "fullScreenModal__close fullScreen" : "fullScreenModal__close"} onClick={toggle}>&times;</span>
-        <div className={open ? "fullScreenModal__caption fullScreen" : "fullScreenModal__caption"}>{imageCaption}</div>
-      </div>
-    </div>
+    
   );
 }
 
